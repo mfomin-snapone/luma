d82f30d022a48e1d1eae38b419178d85c969a114
mark.fomin <mfomin93@gmail.com>
2021-04-14 11:47:24 -0400

    Added IPC Reboot script, renamed and separated PoE Reboot script as well.

---
+++ IPC_Reboot.py
@@ -0,0 +1,113 @@
+import os
+import time
+from selenium.webdriver.common.keys import Keys
+from locators import Elements
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+import datetime
+
+output_file_name = "Logs"
+output_file_path = os.getcwd() + "\\logs\\{}.txt".format(output_file_name)
+outputFile = open(output_file_path, "a+")
+
+
+def get_timestamp():
+    dt = datetime.datetime.now()
+    print_timestamp = dt.strftime("%d-%b-%Y (%H:%M:%S.%f)")
+    return print_timestamp
+
+
+ip_address = input("Enter the device IP address: ")
+url = "http://{}/".format(ip_address)
+loop_counter = input("Enter number of loops: ")
+start_script_choice = 'y'
+script_loop_count = 0
+screenshot_counter = 0
+
+options = Options()
+capabilities = DesiredCapabilities.CHROME
+capabilities["pageLoadStrategy"] = "normal"
+driver = webdriver.Chrome()
+driver.implicitly_wait(10)
+
+if start_script_choice == 'Y' or start_script_choice == 'y':
+    for x in range(0, int(loop_counter)):
+        script_loop_count = script_loop_count + 1
+        print(get_timestamp(), "Starting script! Loop Interval: " + str(script_loop_count))
+        print(get_timestamp(), "Starting script!", file=outputFile)
+        screenshot_counter = screenshot_counter + 1
+        driver.get(url)
+        time.sleep(2)
+
+        # Entering username
+        driver.find_element(*Elements.login_username).send_keys("admin")
+        print(get_timestamp(), "Entering username")
+        print(get_timestamp(), "Entering username", file=outputFile)
+        # Entering password
+        driver.find_element(*Elements.login_password).send_keys("Snapav704")
+        print(get_timestamp(), "Entering password")
+        print(get_timestamp(), "Entering password", file=outputFile)
+        time.sleep(1)
+        # Selecting login button
+        driver.find_element(*Elements.login_button).click()
+        print(get_timestamp(), "Logging in")
+        print(get_timestamp(), "Logging in", file=outputFile)
+        time.sleep(5)
+        driver.find_element(*Elements.settings_button).click()
+        print(get_timestamp(), "Selecting Settings")
+        print(get_timestamp(), "Selecting Settings", file=outputFile)
+        time.sleep(2)
+        driver.find_element(*Elements.maintenance_menu).click()
+        print(get_timestamp(), "Selecting Maintenance menu")
+        print(get_timestamp(), "Selecting Maintenance menu", file=outputFile)
+        time.sleep(2)
+        driver.find_element(*Elements.reboot_button).click()
+        print(get_timestamp(), "Selecting Reboot button")
+        print(get_timestamp(), "Selecting Reboot button", file=outputFile)
+        time.sleep(2)
+        driver.find_element(*Elements.reboot_popup_ok_option).send_keys(Keys.ENTER)
+        print(get_timestamp(), "Device Rebooting! Please wait...")
+        print(get_timestamp(), "Device Rebooting! Please wait...", file=outputFile)
+        time.sleep(60)
+
+        print(get_timestamp(), "Re-launching web browser!")
+        print(get_timestamp(), "Re-launching web browser!", file=outputFile)
+        driver.close()
+        driver.start_session(capabilities=capabilities)
+        driver.get(url)
+        print(get_timestamp(), "Sending DUT address to browser")
+        print(get_timestamp(), "Sending DUT address to browser", file=outputFile)
+        time.sleep(5)
+
+        # Entering username
+        driver.find_element(*Elements.login_username).send_keys("admin")
+        print(get_timestamp(), "Entering username")
+        print(get_timestamp(), "Entering username", file=outputFile)
+        # Entering password
+        driver.find_element(*Elements.login_password).send_keys("Snapav704")
+        print(get_timestamp(), "Entering password")
+        print(get_timestamp(), "Entering password", file=outputFile)
+        time.sleep(1)
+        # Selecting login button
+        driver.find_element(*Elements.login_button).click()
+        print(get_timestamp(), "Login button selected")
+        print(get_timestamp(), "Login button selected", file=outputFile)
+        time.sleep(15)
+        screenshot_file = "Loop " + str(screenshot_counter) + ".png"
+        driver.get_screenshot_as_file('Screenshots\\' + screenshot_file)
+        print(get_timestamp(), "Post-Loop Screenshot taken: " + str(screenshot_counter))
+        print(get_timestamp(), "Post-Loop Screenshot taken: " + str(screenshot_counter), file=outputFile)
+        time.sleep(2)
+        driver.close()
+        driver.start_session(capabilities=capabilities)
+        print(get_timestamp(), "Loop Finished!")
+        print(get_timestamp(), "Loop Finished!", file=outputFile)
+
+
+print(get_timestamp(), "Script Finished!")
+print(get_timestamp(), "Script Finished!", file=outputFile)
+
+driver.quit()
--- resets.py
+++ resets.py
@@ -12,68 +12,85 @@ wb_timeout = 100
 
 def reset_outlet():
     with telnetlib.Telnet(wb_host, wb_port, wb_timeout) as session:
+
         session.write(b"wattbox\n")
         session.write(b"SnapAV704\n")
-        session.write(b"!OutletSet=6,RESET,1")
+        session.write(b"?Firmware\n")
+        session.read_all()   
+
+reset_outlet()
+# session.write(b"!OutletSet=6,RESET,1")
+# def reset_moip_switch():
+#     tn = telnetlib.Telnet(wb_host)
+#     tn.read_until(b"Username: ")
+#     tn.write(b"araknis\n")
+#     if password:
+#         tn.read_until(b"Password: ")
+#     tn.write(b"SnapAV704\n")
+#     tn.write(b"?Firmware\n")
+
+#     print(tn.read_all())
 
 
-def network_port_reset():
-    tn = telnetlib.Telnet(host)
-    tn.read_until(b"Username: ")
-    tn.write(b"araknis\n")
-    if password:
-        tn.read_until(b"Password: ")
-    tn.write(b"SnapAV704\n")
-    time.sleep(1)
-    tn.write(b"configure\n")
-    time.sleep(1)
-    tn.write(b"interface GigabitEthernet 3\n")
-    time.sleep(1)
-    tn.write(b"shutdown\n")
-    time.sleep(10)
-    tn.write(b"no shutdown\n")
-    time.sleep(1)
-    tn.write(b"end\n")
-    time.sleep(1)
-    tn.write(b"exit\n")
+# reset_moip_switch()
+
+# def network_port_reset():
+#     tn = telnetlib.Telnet(host)
+#     tn.read_until(b"Username: ")
+#     tn.write(b"araknis\n")
+#     if password:
+#         tn.read_until(b"Password: ")
+#     tn.write(b"SnapAV704\n")
+#     time.sleep(1)
+#     tn.write(b"configure\n")
+#     time.sleep(1)
+#     tn.write(b"interface GigabitEthernet 3\n")
+#     time.sleep(1)
+#     tn.write(b"shutdown\n")
+#     time.sleep(10)
+#     tn.write(b"no shutdown\n")
+#     time.sleep(1)
+#     tn.write(b"end\n")
+#     time.sleep(1)
+#     tn.write(b"exit\n")
 
 
-def network_port_on():
-    tn = telnetlib.Telnet(host)
-    tn.read_until(b"Username: ")
-    tn.write(b"araknis\n")
-    if password:
-        tn.read_until(b"Password: ")
-    tn.write(b"SnapAV704\n")
-    time.sleep(1)
-    tn.write(b"configure\n")
-    time.sleep(1)
-    tn.write(b"interface GigabitEthernet 3\n")
-    time.sleep(1)
-    tn.write(b"no shutdown\n")
-    time.sleep(1)
-    tn.write(b"end\n")
-    time.sleep(1)
-    tn.write(b"exit\n")
+# def network_port_on():
+#     tn = telnetlib.Telnet(host)
+#     tn.read_until(b"Username: ")
+#     tn.write(b"araknis\n")
+#     if password:
+#         tn.read_until(b"Password: ")
+#     tn.write(b"SnapAV704\n")
+#     time.sleep(1)
+#     tn.write(b"configure\n")
+#     time.sleep(1)
+#     tn.write(b"interface GigabitEthernet 3\n")
+#     time.sleep(1)
+#     tn.write(b"no shutdown\n")
+#     time.sleep(1)
+#     tn.write(b"end\n")
+#     time.sleep(1)
+#     tn.write(b"exit\n")
 
 
-def network_port_off():
-    tn = telnetlib.Telnet(host)
-    tn.read_until(b"Username: ")
-    tn.write(b"araknis\n")
-    if password:
-        tn.read_until(b"Password: ")
-    tn.write(b"SnapAV704\n")
-    time.sleep(1)
-    tn.write(b"configure\n")
-    time.sleep(1)
-    tn.write(b"interface GigabitEthernet 3\n")
-    time.sleep(1)
-    tn.write(b"shutdown\n")
-    time.sleep(1)
-    tn.write(b"end\n")
-    time.sleep(1)
-    tn.write(b"exit\n")
+# def network_port_off():
+#     tn = telnetlib.Telnet(host)
+#     tn.read_until(b"Username: ")
+#     tn.write(b"araknis\n")
+#     if password:
+#         tn.read_until(b"Password: ")
+#     tn.write(b"SnapAV704\n")
+#     time.sleep(1)
+#     tn.write(b"configure\n")
+#     time.sleep(1)
+#     tn.write(b"interface GigabitEthernet 3\n")
+#     time.sleep(1)
+#     tn.write(b"shutdown\n")
+#     time.sleep(1)
+#     tn.write(b"end\n")
+#     time.sleep(1)
+#     tn.write(b"exit\n")
 
 
 def poe_port_reset():
